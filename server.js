56aff694a09c51c7e9fc418779c126a1ff398c25
Insaf <yacoubi.insaf@gmail.com>
2026-04-03 15:07:11 +0200

    Fix variables MySQL Railway

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const mysql = require("mysql2/promise");
 const app = express();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root1234",
-  database: process.env.DB_NAME || "california_gym",
-  port: process.env.DB_PORT || 3306,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
